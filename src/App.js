bf65de252fc5801d9543c2d5f3f56b47d7db6aba
JUHEEBAEK <bjh9287@naver.com>
2021-07-14 00:49:35 +0900

    [feat] 게임 리스트 기본 default 값 만들기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import React, { useRef, useState } from "react";
 import './App.css';
 import InputBox from "./InputBox";
+import GameList from "./GameList";
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -10,6 +11,30 @@ function App() {
   });
 
   const { initial, answer } = inputs;
+
+  const [games, setGames] = useState([
+    {
+      id: 1,
+      initial: "ㅎㄹㅇ",
+      answer: "호랑이"
+    },
+    {
+      id: 2,
+      initial: "ㅇㅅㄹ",
+      answer: "오소리"
+    },
+    {
+      id: 3,
+      initial: "ㅋㄲㄹ",
+      answer: "코끼리"
+    },
+    {
+      id: 4,
+      initial: "ㅋㅇㄹ",
+      answer: "코알라",
+    }
+  ]);
+
   const onChange = e => {
     const { name, value } = e.target;
     setInputs({
